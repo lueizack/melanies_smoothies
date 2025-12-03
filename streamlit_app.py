a8331fbe0f264a1dc3e87fde2a44a49b41482951
Harshal Rajput <lueizack@gmail.com>
2025-12-03 13:26:40 +0530

    Update streamlit_app.py
    Added request to the SniS app

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,9 @@ if time_to_insert:
     
     st.success('Your Smoothie is ordered!',icon="✅")
 
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
     
 
