ba6c92d601194ec6e93bd6a6913e4aae75630f30
Harshal Rajput <lueizack@gmail.com>
2025-12-03 15:01:08 +0530

    Update streamlit_app.py
    sfdf added

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefruit_response.json(),use_container_width=True)
 
     
 
